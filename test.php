beb85e424a67ef4aba382a8cb8e82bd9c4765e07
Meeria <meria.dap@gmail.com>
2022-11-13 23:59:10 +0100

    maj

--- test.php
+++ test.php
@@ -36,13 +36,91 @@ else {
 }
 ?> <br><br>
 
+<?php
+$grade = 16;
 
+switch ($grade) // on indique sur quelle variable on travaille
+{ 
+    case 0: // dans le cas où $grade vaut 0
+        echo "Tu es vraiment un gros nul !!!";
+    break;
+    
+    case 5: // dans le cas où $grade vaut 5
+        echo "Tu es très mauvais";
+    break;
+    
+    case 7: // dans le cas où $grade vaut 7
+        echo "Tu es mauvais";
+    break;
+    
+    case 10: // etc. etc.
+        echo "Tu as pile poil la moyenne, c'est un peu juste…";
+    break;
+    
+    case 12:
+        echo "Tu es assez bon";
+    break;
+    
+    case 16:
+        echo "Tu te débrouilles très bien !";
+    break;
+    
+    case 20:
+        echo "Excellent travail, c'est parfait !";
+    break;
+    
+    default:
+        echo "Désolé, je n'ai pas de message à afficher pour cette note";
+}
+?>
+
+<?php
+$age = 32 ;
+
+if ($age >= 18) {
+    $adult = true ; 
+}
+else {
+    $adult = false ;    
+}
+?>
+<br><br>
+
+<!-- les tableaux -->
 
 <?php
 
+$user1 = ['Mériadec Dap', 'email', 'secret', 34];
+    
+echo $user1[0];
+echo $user1[1];
+echo $user1[3];
+
+?>
+
+<!-- les boucles -->
+<?php
+
+$line = 1;
+
+while ($line <= 10){
+    echo 'je ne dois pas discuter en classe.<br />'; 
+    $line = $line + 1;
+}
+
+?>
+
+<?php
+$line = 1;
+
+while ($line <= 10){
+    echo 'Ceci est la ligne n°'. $line . '<br />'; 
+    $line = $line + 1;
+}
 
 
 
+?>
 ?>
 
 </body>
